509d51057f0d6f0de3e236ba1c50686fcfd6cc03
Bal-Tzar <ottodemare1@gmail.com>
2025-04-30 11:30:51 +0200

    commit to be able to rebase

--- src/MerchStore.Application/Services/Interfaces/IShoppingCartService.cs
+++ src/MerchStore.Application/Services/Interfaces/IShoppingCartService.cs
@@ -2,7 +2,7 @@ namespace MerchStore.Service.ShoppingCart
 {
     public interface IShoppingCartService
     {
-        Task<Domain.ShoppingCart.ShoppingCart> GetCartAsync(Guid cartId);
+        Task GetCartAsync(Guid cartId);
         Task<bool> AddItemToCartAsync(Guid cartId, string productId, int quantity);
         Task<bool> RemoveItemFromCartAsync(Guid cartId, string productId);
         Task<bool> UpdateItemQuantityAsync(Guid cartId, string productId, int quantity);
