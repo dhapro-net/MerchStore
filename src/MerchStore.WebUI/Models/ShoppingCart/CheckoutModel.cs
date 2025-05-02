1d8f68419586fed380eca986beaeadcea5a4fd4b
oskarnilsson-xyz <114058419+oskarnilsson-xyz@users.noreply.github.com>
2025-05-02 15:04:26 +0200

    MERCH-48 : fix duplicate code

--- src/MerchStore.WebUI/Models/ShoppingCart/CheckoutModel.cs
+++ src/MerchStore.WebUI/Models/ShoppingCart/CheckoutModel.cs
@@ -5,19 +5,3 @@ public class CheckoutRequest
     public PaymentInfo Payment { get; set; }
     public decimal TotalPrice { get; set; }
 }
-
-public class ShippingInfo
-{
-    public string FullName { get; set; }
-    public string Address { get; set; }
-    public string City { get; set; }
-    public string PostalCode { get; set; }
-    public string Country { get; set; }
-}
-
-public class PaymentInfo
-{
-    public string CardNumber { get; set; }
-    public string ExpirationDate { get; set; }
-    public string CVV { get; set; }
-}
