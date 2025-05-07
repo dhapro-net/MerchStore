40643140f4b33a6e995576b3881b616fcd782791
Bal-Tzar <ottodemare1@gmail.com>
2025-05-07 09:33:23 +0200

    Added domain events infrastructure

---
+++ src/MerchStore.Domain/Common/AggregateRoot.cs
@@ -0,0 +1,23 @@
+namespace MerchStore.Domain.Common;
+
+public abstract class AggregateRoot<TId> : Entity<TId> where TId : notnull
+{
+    private readonly List<DomainEvent> _domainEvents = new();
+    
+    public IReadOnlyCollection<DomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+    
+    protected AggregateRoot(TId id) : base(id) { }
+    
+    // Required for EF Core
+    protected AggregateRoot() { }
+    
+    protected void AddDomainEvent(DomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+    
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+}
---
+++ src/MerchStore.Domain/Common/DomainEvent.cs
@@ -0,0 +1,7 @@
+namespace MerchStore.Domain.Common;
+
+public abstract class DomainEvent
+{
+    public DateTime OccurredOn { get; } = DateTime.UtcNow;
+    public Guid Id { get; } = Guid.NewGuid();
+}
