88a8a75113f0c515ef4b9433f9cf374ad578c66c
Bal-Tzar <ottodemare1@gmail.com>
2025-05-07 10:45:10 +0200

    Feat: Added NuGet packages: MediatR, MediatR Dependency injection, FluentValidation, git add . dependency injection, AspNetCore.Http, git add ..Extensions, Microsoft.Extensions.Http, git add ..Logging

--- src/MerchStore.WebUI/Program.cs
+++ src/MerchStore.WebUI/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddApplication();
 // Add Infrastructure services - this includes DbContext, Repositories, etc.
 builder.Services.AddInfrastructure(builder.Configuration);
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
