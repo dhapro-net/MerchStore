14eea6e747f06fbc0366c34c619d673ffe17c3eb
Yotaka k. <88yotkha@gafe.molndal.se>
2025-05-22 10:51:02 +0200

    MERCH-61 : feat: Implement user authentication and admin management features

---
+++ src/MerchStore.WebUI/Models/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+
+using System.ComponentModel.DataAnnotations;
+namespace MerchStore.WebUI.Models;
+
+
+public class RegisterViewModel
+{
+    [Required]
+    [StringLength(50)]
+    public string Username { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(100, MinimumLength = 6)]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+} 
