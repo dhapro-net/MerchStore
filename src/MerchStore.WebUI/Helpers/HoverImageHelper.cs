a48ed5ca18d21656067c303b2ccaad5f7b6d396c
Yotaka k. <88yotkha@gafe.molndal.se>
2025-05-26 02:50:03 +0200

    fix: Update hover image URLs to use relative paths and remove unused conditions

--- src/MerchStore.WebUI/Helpers/HoverImageHelper.cs
+++ src/MerchStore.WebUI/Helpers/HoverImageHelper.cs
@@ -5,22 +5,13 @@ namespace MerchStore.WebUI.Helpers
         public static string GetHoverImageUrl(string productName)
         {
             if (productName.Contains("Litter cute Mug", StringComparison.OrdinalIgnoreCase))
-                return "https://somethingpicture20250509.blob.core.windows.net/picture/merchimug02.png";
+                return "/img/merchimug02.png";
 
             if (productName.Contains("Laptop Sticker Pack", StringComparison.OrdinalIgnoreCase))
-                return "https://somethingpicture20250509.blob.core.windows.net/picture/sticker01.png";
+                return "/img/sticker01.png";
 
-            if (productName.Contains("Canvas for decorating", StringComparison.OrdinalIgnoreCase))
-                return "https://somethingpicture20250509.blob.core.windows.net/picture/canvas03.png";
-
-            if (productName.Contains("dragon Coaster", StringComparison.OrdinalIgnoreCase))
-                return "https://somethingpicture20250509.blob.core.windows.net/picture/coaster01.png";
-
-            if (productName.Contains("ToteBag Cute bag", StringComparison.OrdinalIgnoreCase))
-                return "https://somethingpicture20250509.blob.core.windows.net/picture/totebag08.png";
-
-            if (productName.Contains("Angry ToteBag", StringComparison.OrdinalIgnoreCase))
-                return "https://somethingpicture20250509.blob.core.windows.net/picture/totebag04.png";
+            if (productName.Contains("Hoodie", StringComparison.OrdinalIgnoreCase))
+                return "/img/hoodie.png";
 
             return "/img/canvas01.png";
         }
